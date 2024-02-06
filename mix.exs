aa7787a60c0e34a1295472596c26e5e9688d6f53
joaop21 <jpsilva9898@gmail.com>
2024-02-06 00:45:41 +0000

    update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Videochat.MixProject do
     [
       app: :videochat,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
